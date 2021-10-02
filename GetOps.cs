c0366ec117796f92509c00f3c515605b55455f26
Robert Lacy <habikki@luvs.org>
2021-10-01 21:16:23 -0700

    Core API

---
+++ GetOps.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NeoAgi.CommandLine
+{
+    public static class StringArrayExtension 
+    {
+        public static T GetOps<T>(this string[] args) where T : new()
+        {
+            return new T();
+        }
+    }
+}
