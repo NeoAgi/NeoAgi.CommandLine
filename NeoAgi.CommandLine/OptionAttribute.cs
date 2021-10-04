1c4c1cd59be80d5bf3ddc77f9680c9981c314fbb
Robert Lacy <habikki@luvs.org>
2021-10-03 21:14:46 -0700

    Adding documentation

--- NeoAgi.CommandLine/OptionAttribute.cs
+++ NeoAgi.CommandLine/OptionAttribute.cs
@@ -1,13 +1,30 @@
 ﻿namespace NeoAgi.CommandLine
 {
+    /// <summary>
+    /// Attribute to tag to Properties that defines how an option should be applied to the POCO
+    /// </summary>
     [AttributeUsage(AttributeTargets.Property)]
     public class OptionAttribute : Attribute
     {
+        /// <summary>
+        /// Short name used to identify if an error occured while parsing.
+        /// </summary>
         public string? FriendlyName { get; set; } = null;
+        /// <summary>
+        /// Short form option flag (e.g. -o), - will be appended automatically
+        /// </summary>
         public string ShortName { get; set; } = string.Empty;
+        /// <summary>
+        /// Long form option flag (e.g. --option), -- will be appended automatically
+        /// </summary>
         public string LongName { get; set; } = string.Empty;
+        /// <summary>
+        /// Concise description to be printed for usage assistance.
+        /// </summary>
         public string Description {  get; set; } = string.Empty;
+        /// <summary>
+        /// Flag indicating if the option must be present for success.
+        /// </summary>
         public bool Required { get; set; } = false;
-        public string NotFoundText { get; set; } = string.Empty;
     }
 }
