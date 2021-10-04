7b3dab97e4e47e704ac0a017ccaaf22394a67182
Robert Lacy <habikki@luvs.org>
2021-10-03 21:05:09 -0700

    ExitDirective no longer needed

--- NeoAgi.CommandLine/ExitDirective.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace NeoAgi.CommandLine
-{
-    public class ExitDirective
-    {
-        public int ExitCode { get; set; } = 1;
-        public bool ProcessHelp { get; set; } = true;
-        public bool KillProcessOnError { get; set; } = true;
-    }
-}
