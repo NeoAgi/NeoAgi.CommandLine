2823f4497914df6b82fa8ecee8f7b3f1b404f84c
Robert Lacy <habikki@luvs.org>
2021-10-03 21:01:52 -0700

    Normalized the API of string[].GetOps<T>() to throw Exceptions than nullables

--- NeoAgi.CommandLine/GetOps.cs
+++ NeoAgi.CommandLine/GetOps.cs
@@ -1,27 +1,30 @@
-﻿namespace NeoAgi.CommandLine
+﻿using System.IO;
+
+namespace NeoAgi.CommandLine
 {
-    public static class StringArrayExtension
+    public static class GetOpsExtension
     {
-        public static T? GetOps<T>(this string[] args, Action<OptionManager, ExitDirective>? func = null) where T : new()
+        public static T? GetOps<T>(this string[] args, TextWriter? output = null, Func<OptionManager, bool>? func = null) where T : new()
         {
-            bool invokeFunc = false;
-            bool invokeMerge = false;
+            // Declare a few initial variabled
+            T retVal = new T();
+            bool printHelp = true;
+            ApplicationException? raised = null;
             OptionManager manager = new OptionManager();
             Dictionary<string, string> dict = new Dictionary<string, string>();
 
+            // First try to parse the arguments, if an exception is raised here, captured it but proceed on
             try
             {
                 dict = manager.Parse(args);
-                invokeMerge = true;
             }
-            catch (RaiseHelpException)
+            catch (RaiseHelpException ex)
             {
-                invokeFunc = true;
+                raised = ex;
             }
 
-            T retVal = new T();
-
-            if (invokeMerge == true)
+            // If we didn't error above, attempt to merge
+            if (raised == null)
             {
                 try
                 {
@@ -30,30 +33,29 @@
                 catch (RequiredOptionNotFoundException ex)
                 {
                     manager.Errors.Add(ex.Option);
-                    invokeFunc = true;
+                    raised = ex;
                 }
             }
 
-            if (invokeFunc)
+            // Regardless if an exception is raised, process the delegate
+            if (func != null)
             {
-                ExitDirective exit = new ExitDirective();
-                if (func != null)
-                {
-                    func.Invoke(manager, exit);
-                }
-
-                if (exit.ProcessHelp)
-                {
-                    manager.PrintHelp<T>(Console.Out, manager.Errors);
-                }
-
-                if (exit.ExitCode != int.MinValue && exit.KillProcessOnError)
-                {
-                    Environment.Exit(exit.ExitCode);
-                }
-
-                return default(T);
+                printHelp = func.Invoke(manager);
             }
+
+            if (printHelp)
+            {
+                if (output == null)
+                {
+                    output = Console.Out;
+                }
+
+                manager.PrintHelp<T>(output, manager.Errors);
+            }
+
+            // If a exception is raised above, re-throw it now
+            if(raised != null)
+                throw raised;
 
             return retVal;
         }
