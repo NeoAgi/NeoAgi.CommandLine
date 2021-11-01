7bd3ff8870d89230114e280b9ea8503111dc284b
Robert Lacy <habikki@luvs.org>
2021-10-31 21:10:02 -0700

    Adding helper for Coverage Report Generation

---
+++ generate-coverage-report.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+reportgenerator -reports:"NeoAgi.CommandLine.Tests/TestResults/6010d59c-3c9d-49ba-bb61-3dc8567aae40/coverage.cobertura.xml" -targetdir:"coveragereport" -reporttypes:Html
